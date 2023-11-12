e5f7596b6922c99d16b55c58083b21d4e69a3106
teamtee <87838510+teamtee@users.noreply.github.com>
2023-11-12 20:13:49 +0800

    Update

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # 加载 nvm
 nvm use 10
-# gitbook install book
+gitbook install book
 gitbook build book docs
 git add . 
 git commit -m "Update"
